1a41f08a6dab52b66c71066a81fb5ac38cb7f654
David Clark <davidatfernlea1992@gmail.com>
2021-04-08 17:07:17 +0200

    correcting find time slot bug

--- src/findTimeSlot.js
+++ src/findTimeSlot.js
@@ -37,22 +37,28 @@ const findTimeSlot = (dayOfweek, {data, fileName}) => {
     let firstAvailableTime = null;
 
     currentDay.forEach((meeting, index) => {
+
+        // if there is space at the beginning of the day
         if (index === 0 && meeting.start >= startOfDay + 60) {
             if (firstAvailableTime === null) {
                 firstAvailableTime = `${fileName}: ${meeting.day} ${timeConvert(startOfDay)}-${timeConvert(startOfDay + 59)}`;
             }
+        }
+
+        // if there is space in the middle of the day
+        if(meeting.start > nextTimeSlot + 60 && meeting.start < endOfDay - 59) {
+            if (firstAvailableTime === null) {
+                firstAvailableTime = `${fileName}: ${meeting.day} ${timeConvert(nextTimeSlot + 1)}-${timeConvert(nextTimeSlot + 60)}`;
+            }
+            nextTimeSlot = updateTimeSlot(meeting.end, nextTimeSlot);
         } else {
-            if (index === 0) {
-                nextTimeSlot = meeting.end;
-            } else if (nextTimeSlot < endOfDay - 60) {
-                if (meeting.start >= (nextTimeSlot + 59)) {
-                    if (firstAvailableTime === null) {
-                        firstAvailableTime = `${fileName}: ${meeting.day} ${timeConvert(nextTimeSlot + 1)}-${timeConvert(nextTimeSlot + 60)}`;
-                    }
-                    nextTimeSlot = updateTimeSlot(meeting.end, nextTimeSlot);
-                } else {
-                    nextTimeSlot = updateTimeSlot(meeting.end, nextTimeSlot);
-                }
+            nextTimeSlot = updateTimeSlot(meeting.end, nextTimeSlot);
+        }
+
+        // if there is space at the end of the day
+        if (index === currentDay.length - 1 && meeting.start <= endOfDay - 59 && nextTimeSlot < endOfDay - 59) {
+            if (firstAvailableTime === null) {
+                firstAvailableTime = `${fileName}: ${meeting.day} ${timeConvert(nextTimeSlot + 1)}-${timeConvert(nextTimeSlot + 60)}`;
             }
         }
     });
